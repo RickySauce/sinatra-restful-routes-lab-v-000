68bbd3ca1fe37ea04099aa847254f39b50aa5345
Eric DeSimone <eric.desimone@cortland.edu>
2018-07-09 12:38:10 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < Sinatra::Base
 
   post '/recipes' do
     @recipe = Recipe.create(params)
-    redirect '/recipes'
+    redirect '/recipes/#{@recipe.id}'
   end
 
   get '/recipes' do
